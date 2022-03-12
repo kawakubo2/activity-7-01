76828923147022e15bea7a638bf8bda47ccefc39
Tomoharu Kawakubo <tomo.kawakubo@gmail.com>
2022-03-12 11:15:28 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import ConvertForm from './ConvertForm';
+import StatusContext from './StatusContext';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: '',
+      celsius: 0,
+      fahrenheit: 0
+    }
+  }
+  getFahrenheit(event) {
+    const celsius = Number.parseFloat(event.target.value);
+    const fahrenheit = (celsius * 9) / 5 + 32;
+    console.log(`getFahrenheit: ${celsius} ---> ${fahrenheit}`);
+    this.setState({ celsius, fahrenheit});
+  }
+  getCelsius(event) {
+    const fahrenheit = Number.parseFloat(event.target.value);
+    const celsius = ((fahrenheit - 32) * 5) / 9;
+    this.setState({ celsius, fahrenheit })
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <StatusContext.Provider value={this.state.celsius}>
+          <ConvertForm
+            celsius={this.state.celsius}
+            fahrenheit={this.state.fahrenheit}
+            getFahrenheit={this.getFahrenheit.bind(this)}
+            getCelsius={this.getCelsius.bind(this)}
+
+          />
+        </StatusContext.Provider>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
